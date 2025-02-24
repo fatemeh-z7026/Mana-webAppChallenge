c90e592ce81d02de8516a26240048ffde2e7065d
F-zeinali <fatemeh.zeinali7026@gmail.com>
2025-02-24 18:41:34 +0330

    add front api request

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let $ = document;
 let resultContainer = $.getElementById("result");
 
+$.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("http://localhost:5001/api/cmd", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        cmd: "GetGeneralFrame",
+        subject: "GetEMSFrame",
+        id: "559",
+        type: "Normal",
+        datetime: "2025-01-06 12:12:12",
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+
 //Create Card
 let card = `<div class="card"><h3>KWH (EMS-Meter)</h3><select>
             <option value="minTotal">Min</option>
